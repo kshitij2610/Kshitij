c8ada4a88739d03acf5c4f936f0887ddb53e9a0f
Kshitij Shukla <98797426+kshitij2610@users.noreply.github.com>
2022-08-05 14:00:36 +0530

    Add files via upload

---
+++ Binary_2.java
@@ -0,0 +1,41 @@
+package com;
+
+public class Binary_2 {
+        static int binarySearch(int arr[], int l, int r, int x){
+
+    if (r>=l)
+    {
+        int mid = (l + r)/2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid-1, x);
+        return binarySearch(arr, mid+1, r, x);
+    }
+    return -1;
+}
+       static int findPos(int arr[],int key)
+        {
+            int l = 0, h = 1;
+            while (arr[h] < key)
+            {
+                l = h;
+                h=2*h;
+            }
+            return binarySearch(arr, l, h, key);
+        }
+
+        // Driver method to test the above function
+        public static void main(String[] args)
+        {
+            int arr[] = new int[]{3, 5, 7, 9, 10, 90,
+                    100, 130, 140, 160, 170};
+            int ans = findPos(arr,10);
+
+            if (ans==-1)
+                System.out.println("Element not found");
+            else
+                System.out.println("Element found at index " + ans);
+        }
+}
+
